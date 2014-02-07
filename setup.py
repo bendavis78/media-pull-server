dff73e367c842c5f591a4034848de75cedf018ba
Ben Davis <bendavis78@gmail.com>
2014-02-07 14:26:50 -0600

    fixed setup.py and overall structure

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='media-pull-server',
     version='0.1dev',
-    py_modules=['mediaserver', 'paramiko'],
-    install_requires=['twisted'],
+    py_modules=['mediaserver'],
+    install_requires=['twisted', 'paramiko'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     entry_points={
